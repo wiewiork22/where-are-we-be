a28241776be889409bb22cc4f1759caaf28d07d0
Szymon Stasiak <stszymek@gmail.com>
2023-11-21 21:11:17 +0000

    Merged in feat/S2ED-86-field-validation-BE (pull request #26)
    Fields validation in BE
    
    * Fields validation in BE
    
    * User can have 2 or more name
    
    
    Approved-by: Izzatulla Toshpulatov
    Approved-by: Oleksandr Verdiyev
    Approved-by: Karol Wiewiór

--- src/main/java/com/employeedashboard/oirs/dto/AddressDTO.java
+++ src/main/java/com/employeedashboard/oirs/dto/AddressDTO.java
@@ -1,5 +1,43 @@
 package com.employeedashboard.oirs.dto;
 
 public record AddressDTO(String street, String city, String state, String postCode, String country, Double lat,
-        Double lng) {
+                         Double lng) {
+
+    public AddressDTO {
+        if (street == null || street.isEmpty()) {
+            throw new IllegalArgumentException("Street cannot be null or empty");
+        }
+
+        if (city == null || city.isEmpty()) {
+            throw new IllegalArgumentException("City cannot be null or empty");
+        }
+
+        if (state == null || state.isEmpty()) {
+            throw new IllegalArgumentException("State cannot be null or empty");
+        }
+
+        if (postCode == null || postCode.isEmpty()) {
+            throw new IllegalArgumentException("Post code cannot be null or empty");
+        }
+
+        if (country == null || country.isEmpty()) {
+            throw new IllegalArgumentException("Country cannot be null or empty");
+        }
+
+        if (lat == null ){
+            throw new IllegalArgumentException("Latitude cannot be null");
+        }
+
+        if (lng == null) {
+            throw new IllegalArgumentException("Longitude cannot be null");
+        }
+
+        if (lat.isNaN() || lat.isInfinite()) {
+            throw new IllegalArgumentException("Latitude must be a valid floating-point number");
+        }
+
+        if (lng.isNaN() || lng.isInfinite()) {
+            throw new IllegalArgumentException("Longitude must be a valid floating-point number");
+        }
+    }
 }
--- src/main/java/com/employeedashboard/oirs/dto/EmployeeRequestDTO.java
+++ src/main/java/com/employeedashboard/oirs/dto/EmployeeRequestDTO.java
@@ -8,4 +8,33 @@ public record EmployeeRequestDTO(
         String department,
         AddressDTO address
 ) {
+
+    public EmployeeRequestDTO {
+        if (fullName == null || fullName.isEmpty()) {
+            throw new IllegalArgumentException("Full name cannot be null or empty");
+        }
+
+        String[] nameParts = fullName.split(" ");
+
+        if (nameParts.length < 2) {
+            throw new IllegalArgumentException("Full name must contain exactly two parts: first name and last name");
+        }
+
+        if (email == null || !email.matches("^[\\w.-]+@[\\w.-]+\\.[a-z]{2,}$") || email.isEmpty()) {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+
+        if (position == null|| position.isEmpty()) {
+            throw new IllegalArgumentException("Position cannot be null");
+        }
+
+        if (squad == null || squad.isEmpty()) {
+            throw new IllegalArgumentException("Squad cannot be null");
+        }
+
+        if (department == null || department.isEmpty()){
+            throw new IllegalArgumentException("Department cannot be null");
+        }
+
+    }
 }
